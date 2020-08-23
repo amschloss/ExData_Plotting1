1b3c4ef059b5adfc3d03d85e0f54c4534837200e
Adam Schloss <amschloss@gmail.com>
2020-08-22 21:07:23 -0500

    Fix data cleaner source path to be relative instead of machine-specific

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 # Read in the cleaned-up data, which is generated if it doesn't already exist
 datafile <- "./data/subset_power.csv"
-if(!file.exists(datafile)) { source('~/R/explor-analysis-project1/data_clean.R') }
+if(!file.exists(datafile)) { source('./data_clean.R') }
 plotdata <- read.csv(datafile)
 
 png("plot1.png")
