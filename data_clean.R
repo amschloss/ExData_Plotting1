c1dc55a43e32c623eab979b90398663e1ceea24f
Adam Schloss <amschloss@gmail.com>
2020-08-22 22:16:56 -0500

    Add proper datetime conversion to data_clean, needed for plots 2-4

--- data_clean.R
+++ data_clean.R
@@ -14,10 +14,14 @@ if(!file.exists(fullfilename)) {
 classes <- c(rep("character", 2), rep("numeric", 7))
 fulltab <- read.table(fullfilename, header=T, sep=";", na.strings = "?", colClasses = classes)
 
-# Subset and reformat the Date column to actually be of Date type
+# Subset
 spectab <- subset(fulltab, Date %in% c("1/2/2007", "2/2/2007"))
 spectab <- rbind(spectab, subset(fulltab, Date == "3/2/2007" & Time == "00:00:00")) # make sure we get Saturday at the end of our time plots
-spectab$Date <- strptime(spectab$Date, "%d/%m/%Y")
+
+# Make the date/time columns actually a proper datetime
+formDate <- strptime(spectab$Date, "%d/%m/%Y")
+spectab$datetime <- as_datetime(paste(formDate, spectab$Time))
+spectab <- spectab[, c(10, 3:9)]
 
 # Write the cleaned subset out
 write.csv(spectab, "./data/subset_power.csv", row.names = F)
