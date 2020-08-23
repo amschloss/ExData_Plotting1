7c015e951b76e7f21e78634541d451a145fffa59
Adam Schloss <amschloss@gmail.com>
2020-08-22 22:52:41 -0500

    Complete plot 2

--- plot2.R
+++ plot2.R
@@ -3,3 +3,9 @@ datafile <- "./data/subset_power.csv"
 if(!file.exists(datafile)) { source('./data_clean.R') }
 plotdata <- read.csv(datafile)
 plotdata$datetime <- strptime(plotdata$datetime, "%Y-%m-%d %H:%M:%S")
+
+# Generate plot 2 - global active power over time
+png("plot2.png")
+with(plotdata, plot(datetime, Global_active_power, type = "l", xlab = "", 
+                    ylab = "Global Active Power (kilowatts)"))
+dev.off()
