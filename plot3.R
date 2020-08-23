f80fab9f0f9fe82d5bbadbac68292c16e20c00b5
Adam Schloss <amschloss@gmail.com>
2020-08-22 22:28:10 -0500

    Add datetime conversion to plots 2-4

--- plot3.R
+++ plot3.R
@@ -2,4 +2,4 @@
 datafile <- "./data/subset_power.csv"
 if(!file.exists(datafile)) { source('./data_clean.R') }
 plotdata <- read.csv(datafile)
-
+plotdata$datetime <- strptime(plotdata$datetime, "%Y-%m-%d %H:%M:%S")
