563a6367c80fdcaee467ff8994a51c14f126d6cc
Adam Schloss <amschloss@gmail.com>
2020-08-22 21:01:40 -0500

    Build plot 1, histogram of Global Active Power

--- plot1.R
+++ plot1.R
@@ -3,3 +3,6 @@ datafile <- "./data/subset_power.csv"
 if(!file.exists(datafile)) { source('~/R/explor-analysis-project1/data_clean.R') }
 plotdata <- read.csv(datafile)
 
+png("plot1.png")
+hist(plotdata$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
